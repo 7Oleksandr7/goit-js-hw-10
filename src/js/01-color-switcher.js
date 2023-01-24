5761d3f01f4cfb6f7ac2e3ae61b7ad4139156239
7Oleksandr7 <040818a@gmail.com>
2023-01-24 21:17:22 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', colorChangeStart);
+stopBtn.addEventListener('click', colorChangeStop);
+
+let intervalId = null;
+
+function colorChangeStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function colorChangeStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  position: 'center-top',
+  opacity: 0.8,
+  timeout: 2500,
+  clickToClose: true,
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
